097b3cf5b724770f6ea1205e761b42bc473497f9
PhillipHenry <PhillHenry@gmail.com>
2023-06-16 16:44:47 +0100

    Documentation

--- modules/core/src/main/scala/uk/co/odinconsultants/ZioFibers.scala
+++ modules/core/src/main/scala/uk/co/odinconsultants/ZioFibers.scala
@@ -6,8 +6,8 @@ import zio.*
 object ZioFibers extends ZIOAppDefault {
   override def run: ZIO[Any & ZIOAppArgs & Scope, Any, Any] = for {
     _ <- (ZIO.logInfo("Sleeping") *> ZIO.sleep(5.seconds) *> ZIO.logInfo("woken")).fork
-    _ <- ZIO.logInfo("heartbeat").repeat(Schedule.fixed(1.second))
+    _ <- ZIO.logInfo("heartbeat").repeat(Schedule.fixed(1.second)) // .fork this terminates things quickly
   } yield {
-    println("yield")
+    println("yield") // this is never reached
   }
 }
