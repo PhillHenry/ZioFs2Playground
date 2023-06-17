d8cc8148a30776ffd9107ca99cfb982ef6d966cd
PhillipHenry <PhillHenry@gmail.com>
2023-06-17 09:31:29 +0100

    Documentation

--- modules/core/src/main/scala/uk/co/odinconsultants/CatFibers.scala
+++ modules/core/src/main/scala/uk/co/odinconsultants/CatFibers.scala
@@ -3,14 +3,23 @@ package uk.co.odinconsultants
 import cats.effect.{IO, IOApp, OutcomeIO}
 
 import scala.concurrent.duration.*
+import cats.effect.kernel.Outcome.*
 
 object CatFibers extends IOApp.Simple {
 
-  def run: IO[Unit]                  = for {
-    _ <- (IO.println("Sleeping") *> IO.sleep(5.second) *> IO.println("Awake")).background.use {(_: IO[OutcomeIO[Unit]]) =>
-      (IO.println("heartbeat") *> IO.sleep(1.second)).foreverM
-    }
-  } yield {
-    println("yield") // never reached
-  }
+  def run: IO[Unit] = for {
+    _ <- (IO.println("Sleeping") *> IO.sleep(5.second) *> IO.println("Awake")).background.use {
+           (outcome: IO[OutcomeIO[Unit]]) =>
+             val heartbeats: IO[Nothing] = (IO.println("heartbeat") *> IO.sleep(1.second)).foreverM
+             val end: IO[Unit]           = outcome.flatMap { (outcome: OutcomeIO[Unit]) =>
+               val end: IO[Unit] = outcome match {
+                 case Succeeded(_) => IO.println("succeeded")
+                 case _            => IO.println("didn't succeed")
+               }
+               end
+             }
+//             heartbeats *> end // end is never reached
+             heartbeats *> end
+         }
+  } yield println("yield") // never reached
 }
