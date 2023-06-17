2b580dabbc2335add0eaad73287f592b98e05d89
PhillipHenry <PhillHenry@gmail.com>
2023-06-17 09:35:26 +0100

    Documentation

--- modules/core/src/main/scala/uk/co/odinconsultants/CatResourceThreads.scala
+++ modules/core/src/main/scala/uk/co/odinconsultants/CatResourceThreads.scala
@@ -5,21 +5,21 @@ import cats.effect.{IO, IOApp, OutcomeIO}
 import scala.concurrent.duration.*
 import cats.effect.kernel.Outcome.*
 
-object CatFibers extends IOApp.Simple {
+object CatResourceThreads extends IOApp.Simple {
 
   def run: IO[Unit] = for {
     _ <- (IO.println("Sleeping") *> IO.sleep(5.second) *> IO.println("Awake")).background.use {
            (outcome: IO[OutcomeIO[Unit]]) =>
              val heartbeats: IO[Nothing] = (IO.println("heartbeat") *> IO.sleep(1.second)).foreverM
-             val end: IO[Unit]           = outcome.flatMap { (outcome: OutcomeIO[Unit]) =>
+             val leFin: IO[Unit]         = outcome.flatMap { (outcome: OutcomeIO[Unit]) =>
                val end: IO[Unit] = outcome match {
                  case Succeeded(_) => IO.println("succeeded")
                  case _            => IO.println("didn't succeed")
                }
                end
              }
-//             heartbeats *> end // end is never reached
-             heartbeats *> end
+//             heartbeats *> end // end is never reached, heartbeats for ever
+             leFin *> heartbeats // Sleeping, Awake, then forever heartbeats
          }
   } yield println("yield") // never reached
 }
